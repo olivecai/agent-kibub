017f11022e93a9c0867a73fd2f4d2537afbe73ef
olivecai88 <ocai@ualberta.ca>
2026-07-07 14:26:12 +0200

    try pick up cube

--- cluster/TrainKibub-VARS.sh
+++ cluster/TrainKibub-VARS.sh
@@ -9,19 +9,19 @@
 # Run `. TrainKibub-VARS.sh`. Do not run `./TrainKibub-VARS.sh`, as this script is a child process that cannot set variables in its parent.
 
 # dataset variables
-export DATASET_REPO="oliveoil8888/pick-place-cube-cup-1" # hf tag of the Huggingface dataset you would like to download for training
-export TASK="Pick up the cube and place it in the cup." # language prompt demonstrated by the dataset
+export DATASET_REPO="oliveoil8888/pick-up-cube" # hf tag of the Huggingface dataset you would like to download for training
+export TASK="Pick up the cube." # language prompt demonstrated by the dataset
 
 # huggingface auth 
 export HF_USER="oliveoil8888" # your huggingface usertag 
 
 # training variables
-export STEPS=50000
+export STEPS=20000
 export BATCH_SIZE=4
-export MODEL_REPO="pick-up-cup-model" #the name of your resulting model
+export MODEL_REPO="pick-up-cube-model" #the name of your resulting model
 
 # SLURM directives
-export SLURM_TIME="00:05:00"
+export SLURM_TIME="05:00:00"
 
 # Edit no further! Only modify vars above. Do not edit the vars below! 
 ##########################################
